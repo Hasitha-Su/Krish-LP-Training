70e4593c9d81fb663eae4649285f2861c256722e
Hasitha Subhashana <hasithakms@gmail.com>
2021-05-31 23:54:30 +0530

    Update checkAnagram.js

--- Algorithems/Assignments/Assignment2/task-1-Anagram/checkAnagram.js
+++ Algorithems/Assignments/Assignment2/task-1-Anagram/checkAnagram.js
@@ -2,9 +2,8 @@ const checkAnagram = (word11, word22)=>{
 
 	//split for a array	
 	let word1 = word11.split('')
-	let word2 =	word22.split('')
-
-	//word1[0] = word2[0]
+	let word2 = word22.split('')
+	
 	// sort the array
 	word1.sort()
 	word2.sort()
@@ -23,17 +22,4 @@ const checkAnagram = (word11, word22)=>{
 	}
 }
 	
-
-/*
-let word1 = word01.split('')
-
- for(let a=0; a<word1.length - 1; a++){
-	for(let b=0; b<word1.length - 1 - a; b++){
-		if(word1[b+1] < word1[b])
-			[word1[b], word1[b+1]] = [word1[b+1], word1[b]]
-    }
-    console.log(word1)   
-}
-*/
-
 module.exports = {checkAnagram};
